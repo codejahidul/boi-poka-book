09f45079494789697d5ca82271c13f9ad5cddf59
Jahidul <code.jahidul@gmail.com>
2025-03-22 17:25:47 +0600

    added navbar and footer

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,12 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Root from './components/Root/Root';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root> </Root>,
   },
 ]);
 
